dfd5e2c254256a3d6f73a868490883843849d87e
ddk <dattimio95@gmail.com>
2026-03-23 16:18:52 +0100

    fix sticky table header gaps

--- src/components/calculation-log.tsx
+++ src/components/calculation-log.tsx
@@ -22,104 +22,104 @@ function CalculationLog({ resultTimeline }: CalculationLogProps) {
         <TableRow>
           <TableHead
             colSpan={4}
-            className="sticky left-px top-0 z-30 bg-card column-header py-1 text-center text-[16px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]"
+            className="sticky left-0 top-0 z-30 column-header py-1 bg-card border-b border-card text-center shadow-[inset_0_-1px_0_rgb(39,39,42)]"
           >
             Entry
           </TableHead>
           <TableHead
             colSpan={5}
-            className="sticky left-0 top-0 z-20 bg-zinc-800/50 column-header py-1 text-center text-[16px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]"
+            className="sticky left-0 top-0 z-20 column-header py-1 bg-zinc-800 text-center shadow-[inset_0_-1px_0_rgb(39,39,42)]"
           >
             Result
           </TableHead>
           <TableHead
             colSpan={2}
-            className="sticky left-0 top-0 z-20 bg-card column-header py-1 text-center text-[16px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]"
+            className="sticky left-0 top-0 z-20 column-header py-1 bg-card border-b border-card text-center shadow-[inset_0_-1px_0_rgb(39,39,42)]"
           >
             Buffs
           </TableHead>
           <TableHead
             colSpan={6}
             style={getStatCellColor("atk", 0.2)}
-            className="sticky left-0 top-0 z-20 bg-zinc-800/50 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 bg-zinc-800/50 text-center"
           >
             Final Stats
           </TableHead>
           <TableHead
             colSpan={5}
             style={getStatCellColor("basic", 0.2)}
-            className="sticky left-0 top-0 z-20 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 text-center"
           >
             Damage Bonuses
           </TableHead>
           <TableHead
             colSpan={6}
             style={getStatCellColor("aero", 0.2)}
-            className="sticky left-0 top-0 z-20 bg-zinc-800/50 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 bg-zinc-800/50 text-center"
           >
             Elemental Bonuses
           </TableHead>
           <TableHead
             colSpan={5}
             style={getStatCellColor("baDeep", 0)}
-            className="sticky left-0 top-0 z-20 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 text-center"
           >
             Damage Deepen
           </TableHead>
           <TableHead
             colSpan={6}
             style={getStatCellColor("aeDeep", 0)}
-            className="sticky left-0 top-0 z-20 bg-zinc-800/50 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 bg-zinc-800/50 text-center"
           >
             Elemental Deepen
           </TableHead>
           <TableHead
             colSpan={3}
             style={getStatCellColor("bonus", 0)}
-            className="sticky left-0 top-0 z-20 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 text-center"
           >
             Skill Specific
           </TableHead>
           <TableHead
             colSpan={2}
-            className="sticky left-0 top-0 z-20 bg-zinc-800/50 column-header py-1 text-center text-[16px]!"
+            className="sticky left-0 top-0 z-20 column-header py-1 bg-zinc-800/50 text-center"
           >
             Enemy
           </TableHead>
         </TableRow>
         {/* Table headers */}
         <TableRow>
-          <TableHead className="sticky left-px top-6 z-30 bg-card column-header py-1 text-[14px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-30 column-header py-1 bg-card text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             #
           </TableHead>
-          <TableHead className="sticky left-6 top-6 z-30 bg-card min-w-28 column-header py-1 text-[14px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-5.75 top-6 z-30 min-w-28 column-header py-1 bg-card text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Character
           </TableHead>
-          <TableHead className="sticky left-34 top-6 z-30 bg-card column-header py-1 text-[14px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-33.75 top-6 z-30 column-header py-1 bg-card text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             {/* Category */}
           </TableHead>
-          <TableHead className="sticky left-44.25 top-6 z-30 min-w-64 bg-card column-header py-1 text-[14px]! shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-44 top-6 z-30 min-w-64 column-header py-1 bg-card text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Skill
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 text-[14px]! bg-zinc-800/50 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 bg-zinc-800 text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Time
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 text-[14px]! bg-zinc-800/50 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 bg-zinc-800 text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Con.
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 text-[14px]! bg-zinc-800/50 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 bg-zinc-800 text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Res.
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 min-w-16 column-header py-1 text-[14px]! bg-zinc-800/50 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 min-w-16 column-header py-1 bg-zinc-800 text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Damage
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 text-[14px]! bg-zinc-800/50 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 bg-zinc-800 text-[14px]! shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Procc
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 bg-card column-header py-1 text-[14px]! min-w-70 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 bg-card text-[14px]! min-w-70 shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Personal Buffs
           </TableHead>
-          <TableHead className="sticky left-0 top-6 z-20 bg-card column-header py-1 text-[14px]! min-w-70 shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+          <TableHead className="sticky left-0 top-6 z-20 column-header py-1 bg-card text-[14px]! min-w-70 shadow-[inset_0_-1px_0_rgb(39,39,42)]">
             Global Buffs
           </TableHead>
           {statKeys.map((key) => {
@@ -127,7 +127,7 @@ function CalculationLog({ resultTimeline }: CalculationLogProps) {
             return (
               <TableHead
                 key={statKey}
-                className="sticky left-0 top-6 z-20 min-w-16 column-header py-1 text-[14px]! text-center"
+                className="sticky left-0 top-6 z-20 min-w-16 column-header py-1 bg-card text-[14px]! text-center"
               >
                 {STAT_COLORS[statKey].label}
               </TableHead>
@@ -165,12 +165,12 @@ function CalculationLog({ resultTimeline }: CalculationLogProps) {
 
         return (
           <TableRow className="bg-card">
-            <TableCell className="sticky left-px z-10 pt-2! pl-1 pr-2 bg-card text-right text-[12px] text-muted-foreground shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+            <TableCell className="sticky left-0 z-10 pt-2! pl-1 pr-2 bg-card text-right text-[12px] text-muted-foreground shadow-[inset_0_-1px_0_rgb(39,39,42)]">
               {row}
             </TableCell>
             <TableCell
               className={cn(
-                "sticky left-6 z-10 bg-card capitalize shadow-[inset_0_-1px_0_0_rgb(39,39,42)]",
+                "sticky left-5.75 z-10 bg-card capitalize shadow-[inset_0_-1px_0_rgb(39,39,42)]",
                 elementColorText,
               )}
             >
@@ -178,7 +178,7 @@ function CalculationLog({ resultTimeline }: CalculationLogProps) {
             </TableCell>
             <TableCell
               className={cn(
-                "sticky left-34 z-10 pt-2! bg-card align-middle text-[12px] font-mono uppercase tracking-wider shadow-[inset_0_-1px_0_0_rgb(39,39,42)]",
+                "sticky left-33.75 z-10 pt-2! bg-card align-middle text-[12px] font-mono uppercase tracking-wider shadow-[inset_0_-1px_0_rgb(39,39,42)]",
                 isHit ? "text-transparent" : "text-muted-foreground",
               )}
             >
@@ -186,25 +186,25 @@ function CalculationLog({ resultTimeline }: CalculationLogProps) {
             </TableCell>
             <TableCell
               className={cn(
-                "sticky left-44.25 z-10 bg-card shadow-[inset_0_-1px_0_0_rgb(39,39,42)]",
+                "sticky left-44 z-10 bg-card shadow-[inset_0_-1px_0_rgb(39,39,42)]",
                 isCast ? elementColorText : "",
                 isHit ? "text-xs" : "",
               )}
             >
               {skill.name}
             </TableCell>
-            <TableCell className="bg-zinc-800/50 text-right shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+            <TableCell className="bg-zinc-800/50 text-right shadow-[inset_0_-1px_0_rgb(39,39,42)]">
               {frameToSecond(time)}
             </TableCell>
-            <TableCell className="bg-zinc-800/50 text-right shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+            <TableCell className="bg-zinc-800/50 text-right shadow-[inset_0_-1px_0_rgb(39,39,42)]">
               {concerto.toFixed(1)}
             </TableCell>
-            <TableCell className="bg-zinc-800/50 text-right shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+            <TableCell className="bg-zinc-800/50 text-right shadow-[inset_0_-1px_0_rgb(39,39,42)]">
               {resonance.toFixed(1)}
             </TableCell>
             <TableCell
               className={cn(
-                "bg-zinc-800/50 pr-3! text-right shadow-[inset_0_-1px_0_0_rgb(39,39,42)]",
+                "bg-zinc-800/50 pr-3! text-right shadow-[inset_0_-1px_0_rgb(39,39,42)]",
                 damage !== 0 ? elementColorText : "",
               )}
             >
@@ -212,16 +212,16 @@ function CalculationLog({ resultTimeline }: CalculationLogProps) {
             </TableCell>
             <TableCell
               className={cn(
-                "bg-zinc-800/50 pr-2! text-right shadow-[inset_0_-1px_0_0_rgb(39,39,42)]",
+                "bg-zinc-800/50 pr-2! text-right shadow-[inset_0_-1px_0_rgb(39,39,42)]",
                 proc.damage !== 0 ? elementColorText : "",
               )}
             >
               {proc.damage > 0 ? Math.round(proc.damage) : ""}
             </TableCell>
-            <TableCell className="shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+            <TableCell className="text-[14px] shadow-[inset_0_-1px_0_rgb(39,39,42)]">
               {`(${buffs.length || 0}) ${[activeBuffString]}`}
             </TableCell>
-            <TableCell className="shadow-[inset_0_-1px_0_0_rgb(39,39,42)]">
+            <TableCell className="text-[14px] shadow-[inset_0_-1px_0_rgb(39,39,42)]">
               {`(${buffsTeam.length || 0}) ${[activeTeamBuffString]}`}
             </TableCell>
             {stats.map(([statKey, value]) => (
