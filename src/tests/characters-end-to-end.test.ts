7be0b5ac444d4d74bb0943932614287122558a8b
ddk <dattimio95@gmail.com>
2026-03-24 22:53:14 +0100

    feat: add test suite for "Sanhua"

--- src/tests/characters-end-to-end.test.ts
+++ src/tests/characters-end-to-end.test.ts
@@ -9,7 +9,7 @@ import { totalBuffMap } from "@/constants/maps"
 
 describe("Characters: end-to-end", () => {
   const baseBuffMap = totalBuffMap
-  describe("test: Encore", () => {
+  describe("Encore", () => {
     const characterId = "encore"
     const template = characterTemplate[characterId]
     const characterSettings = {
@@ -83,9 +83,7 @@ describe("Characters: end-to-end", () => {
 
     it("S1: test buffs", () => {
       const sequence = 1
-      const team: Character[] = [
-        { ...characters[0], sequence },
-      ]
+      const team: Character[] = [{ ...characters[0], sequence }]
 
       const skill1 = skills.basic[1]
       const skill2 = skills.basic[2]
@@ -136,9 +134,7 @@ describe("Characters: end-to-end", () => {
 
     it("S2: test buffs", () => {
       const sequence = 2
-      const team: Character[] = [
-        { ...characters[0], sequence },
-      ]
+      const team: Character[] = [{ ...characters[0], sequence }]
 
       const skill1 = skills.basic[1]
 
@@ -162,9 +158,7 @@ describe("Characters: end-to-end", () => {
 
     it("S3: test buffs", () => {
       const sequence = 3
-      const team: Character[] = [
-        { ...characters[0], sequence },
-      ]
+      const team: Character[] = [{ ...characters[0], sequence }]
 
       const skill1 = skills.forte[1]
       const skill2 = skills.forte[2]
@@ -189,9 +183,7 @@ describe("Characters: end-to-end", () => {
 
     it("S4: test buffs", () => {
       const sequence = 4
-      const team: Character[] = [
-        { ...characters[0], sequence },
-      ]
+      const team: Character[] = [{ ...characters[0], sequence }]
       const skill1 = skills.forte[2]
 
       if (!skill1) return
@@ -216,9 +208,7 @@ describe("Characters: end-to-end", () => {
 
     it("S5: test buffs", () => {
       const sequence = 5
-      const team: Character[] = [
-        { ...characters[0], sequence },
-      ]
+      const team: Character[] = [{ ...characters[0], sequence }]
       const skill1 = skills.basic[1]
 
       if (!skill1) return
@@ -240,11 +230,10 @@ describe("Characters: end-to-end", () => {
       expect.soft(buffsOnCast).toContain("Hero Takes the Stage!")
       expect(buffMapOnCast.skill).toBeCloseTo(0.35)
     })
+
     it("S6: test buffs", () => {
       const sequence = 6
-      const team: Character[] = [
-        { ...characters[0], sequence },
-      ]
+      const team: Character[] = [{ ...characters[0], sequence }]
       const skill1 = skills.liberation[1]
       const skill2 = skills.basic[6]
       const skill3 = skills.basic[7]
@@ -296,6 +285,211 @@ describe("Characters: end-to-end", () => {
       expect.soft(result[7].buffs).toContain("Woolies Save the World! x5")
 
       expect(result[7].buffMap.atk).toBeGreaterThan(0.8) // at max stacks: +25% atk
+    })
+  })
+
+  describe("Sanhua", () => {
+    const characterId = "sanhua"
+    const template = characterTemplate[characterId]
+    const characterSettings = {
+      sequence: 0,
+      weapon: template.weapon,
+      echoSet: template.echoSet,
+      echo: template.echo,
+    } satisfies CharSettings
+    const characters: Character[] = [
+      computeBaseCharacter(characterId, characterSettings),
+    ]
+    const skills = skillData[characterId]
+
+    const onCast = 0
+
+    it("Basic: test damage", () => {
+      const team = characters
+      const skill = skills.basic[1]
+
+      if (!skill) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill,
+          time: 0,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const totalDamage = result.reduce((total, row) => total + row.damage, 0)
+      const buffsOnCast = result[onCast].buffs
+
+      expect(totalDamage).toBeGreaterThan(0)
+      expect.soft(buffsOnCast).toContain("Moonlit Clouds 2pc")
+    })
+
+    it("Inherent 1: test buffs", () => {
+      const team = characters
+      const skill1 = skills.intro[1]
+
+      if (!skill1) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill: skill1,
+          time: 0,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const buffsFirstHit = result[1].buffs
+      const buffMapFirstHit = result[1].buffMap
+
+      expect(buffsFirstHit).toContain("Ice Thorn")
+      expect(buffsFirstHit).toContain("Condensation")
+      expect(buffMapFirstHit.skill).toBeGreaterThan(0.2)
+    })
+
+    it("Inherent 2: test buffs", () => {
+      const team = characters
+      const skill1 = skills.basic[5]
+      const skill2 = skills.forte[1]
+
+      if (!(skill1 && skill2)) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill: skill1,
+          time: 0,
+        },
+        {
+          characterId,
+          skill: skill2,
+          time: 30,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const buffsFirstHit = result[1].buffs
+      const buffMapDetonateHit = result[3].buffMap
+
+      expect(buffsFirstHit).toContain("Avalanche")
+      expect(buffMapDetonateHit.bonus).toBeCloseTo(0.2)
+    })
+
+    it("S1: test buffs", () => {
+      const sequence = 1
+      const team: Character[] = [{ ...characters[0], sequence }]
+      const skill1 = skills.basic[5]
+
+      if (!skill1) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill: skill1,
+          time: 0,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const buffsFirstHit = result[1].buffs
+      const buffMapFirstHit = result[1].buffMap
+
+      expect(buffsFirstHit).toContain("Solitude's Embrace")
+      expect(buffMapFirstHit.crit).toBeGreaterThan(0.8) // base is 67% + 15% fusion
+    })
+
+    it("S4: test buffs", () => {
+      const sequence = 4
+      const team: Character[] = [{ ...characters[0], sequence }]
+      const skill1 = skills.liberation[1]
+      const skill2 = skills.forte[1]
+
+      if (!(skill1 && skill2)) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill: skill1,
+          time: 0,
+        },
+        {
+          characterId,
+          skill: skill2,
+          time: 30,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const buffsOnCast = result[onCast].buffs
+      const buffsFirstHit = result[1].buffs
+      const buffMapDetonateHit = result[3].buffMap
+
+      expect(buffsOnCast).toContain("Blade Mastery")
+      expect(buffsOnCast).toContain("Blade Mastery (energy)")
+      expect(buffsFirstHit).toContain("Ice Glacier")
+      expect(buffMapDetonateHit.bonus).toBeCloseTo(1.2)
+    })
+
+    it("S5: test buffs", () => {
+      const sequence = 5
+      const team: Character[] = [{ ...characters[0], sequence }]
+      const skill1 = skills.forte[1]
+
+      if (!skill1) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill: skill1,
+          time: 0,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const buffMapFirstHit = result[1].buffMap
+      const buffMapSecondHit = result[2].buffMap
+
+      expect(buffMapFirstHit.critDmg).toBeGreaterThan(3.5)
+      expect(buffMapSecondHit.critDmg).toBeGreaterThan(3.5)
+    })
+
+    it("S6: test buffs", () => {
+      const sequence = 6
+      const team: Character[] = [{ ...characters[0], sequence }]
+      const skill1 = skills.forte[1]
+
+      if (!skill1) return
+
+      const actionList: ActionListItem[] = [
+        {
+          characterId,
+          skill: skill1,
+          time: 0,
+        },
+      ]
+
+      const eventTimeline = computeEventTimeline(actionList)
+      const result = calculate(team, eventTimeline, baseBuffMap)
+
+      const buffsTeamFirstHit = result[1].buffsTeam
+      const buffsTeamSecondHit = result[2].buffsTeam
+
+      expect(buffsTeamFirstHit).toContain("Daybreak Radiance x1")
+      expect(buffsTeamSecondHit).toContain("Daybreak Radiance x2") // TODO: correct procc req
     })
   })
 })
